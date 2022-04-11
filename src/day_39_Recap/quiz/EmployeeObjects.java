bb121b21740ccac7befc170de36e13fcdd940b3f
Mehmet <mehmetnobel26@gmail.com>
2022-04-11 15:21:48 +0300

    day-39 Recap- ShapeTask

---
+++ src/day_39_Recap/quiz/EmployeeObjects.java
@@ -0,0 +1,18 @@
+package day_39_Recap.quiz;
+
+public class EmployeeObjects {
+
+
+    public static void main(String[] args) {
+
+
+        Employee e1=new Employee("ahmet",42);
+
+        System.out.println("e1 = " + e1);
+
+
+
+
+
+    }
+}
---
+++ src/day_39_Recap/quiz/Test.java
@@ -0,0 +1,16 @@
+package day_39_Recap.quiz;
+
+public class Test {
+
+    public Test() {
+        System.out.println("default");
+    }
+
+    public static void main(String[] args) {
+
+        Test obj = new Test();
+
+    }
+
+
+}
