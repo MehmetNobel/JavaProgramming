33b0c621a9a675a29c58b0178102f5349311cc8a
Mehmet <mehmetnobel26@gmail.com>
2022-04-17 14:14:02 +0300

    *Multi-Try Catch Blocks' Examples Added *Finally Block added

--- src/day_41_Exceptions/FinallyBlock.java
+++ src/day_41_Exceptions/FinallyBlock.java
@@ -10,6 +10,7 @@ public class FinallyBlock {
             System.out.println(arr[1]);
         } catch (RuntimeException e) {
             e.printStackTrace();
+            //System.exit(0);  ıf we wanna explicitly terminate the program before finally block then we must use System.Exit(0) method.
         }finally {                      // finally block it is always gets executed, regardless of the exceptions occurs or not
             System.out.println("this is finally block");
         }
