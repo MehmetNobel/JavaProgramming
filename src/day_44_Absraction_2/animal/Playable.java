616a13ad40daa86c04ddb6cf862000f48e0d2c39
Mehmet <mehmetnobel26@gmail.com>
2022-04-26 12:17:37 +0300

    day-44 Interface added

--- src/day_44_Absraction_2/animal/Playable.java
+++ src/day_44_Absraction_2/animal/Playable.java
@@ -2,17 +2,19 @@ package day_44_Absraction_2.animal;
 
 public interface Playable {
 
-     void playable();  // by default  method is already defined as public abstractint
+     void playable();  // by default  method is already defined as public abstract.
 
    //  int a=5;   by default it will be defined as static and final
 
       static void method1(){
           System.out.println("can eat meat-1");
-     }
+     } // must be called via Interface name
 
      default void method2(){
           System.out.println("can eat meat-2");
-     }
+     }  // must be called via object
+
+
 
 
 
