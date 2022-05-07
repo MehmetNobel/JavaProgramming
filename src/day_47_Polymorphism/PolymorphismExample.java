d81ea62a13d6154697ca17e9778030e0536dba97
Mehmet <mehmetnobel26@gmail.com>
2022-05-07 14:25:37 +0300

    day-46 Polymorphism Example

---
+++ src/day_47_Polymorphism/PolymorphismExample.java
@@ -0,0 +1,101 @@
+package day_47_Polymorphism;
+
+
+class Person{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void whatsdoing(){
+
+        System.out.println(getName() + " is ");
+
+    }
+
+
+}
+
+
+class Father extends Person{
+
+
+
+    public Father(String name){
+        setName(name);
+    }
+
+    @Override
+    public void whatsdoing() {
+        System.out.println("father "+ getName() + " is studing java ");
+    }
+}
+
+
+class Mother extends Person{
+
+    public Mother(String name){
+        setName(name);
+    }
+
+    @Override
+    public void whatsdoing() {
+        System.out.println("mother "+ getName() + " is preparing exam for the Math students ");
+    }
+}
+
+
+class Son extends Person{
+
+    public Son(String name){
+        setName(name);
+    }
+
+    @Override
+    public void whatsdoing() {
+        System.out.println("son "+ getName() + " is playing chess ");
+    }
+}
+
+class Daughter extends Person{
+
+    public Daughter(String name){
+        setName(name);
+    }
+
+    @Override
+    public void whatsdoing() {
+        System.out.println("Daughter "+ getName() + " is studying English ");
+    }
+}
+
+
+public class PolymorphismExample {
+
+    public static void whoAndWhat(Person person){
+
+        person.whatsdoing();
+    }
+
+
+
+    public static void main(String[] args) {
+
+        whoAndWhat(new Father("Mehmet"));
+
+        whoAndWhat(new Daughter("Zeynep"));
+
+        whoAndWhat(new Son("Esat"));
+
+        whoAndWhat(new Mother("Ayşe"));
+
+
+
+    }
+}
---
+++ src/day_47_Polymorphism/ReferenceTypeObjects.java
@@ -0,0 +1,43 @@
+package day_47_Polymorphism;
+
+import day_37_Inheritance_2.animal.Animal;
+import day_37_Inheritance_2.animal.Cat;
+import day_37_Inheritance_2.animal.Dog;
+import day_37_Inheritance_2.scrumTask.Developer;
+import day_37_Inheritance_2.scrumTask.Employee;
+import day_37_Inheritance_2.scrumTask.Tester;
+import day_45_Abstraction_2.shapeTask.Circle;
+import day_45_Abstraction_2.shapeTask.Shape;
+
+public class ReferenceTypeObjects {
+
+    public static void main(String[] args) {
+
+        Shape shape=new Circle(4); // there is upcasting here. from sub ==>> to the parent.
+
+        // without casting we cannot convert the sub to the parent.
+
+        System.out.println("shape.area() = " + shape.area());
+
+        Animal animal=new Dog("bed","husky",'m',5,"medium","black");
+
+        Cat cat=new Cat("bed","husky",'m',5,"medium","black");
+
+        ((Dog) animal).bark();  // now we are able to achieve the the dog's proporties. via down casting.
+
+        if(cat instanceof Animal)   // cat bir animal dir
+            System.out.println(true);
+        if(animal instanceof Animal)   // burdaki animal dog u ifade ediyor. dog bir animaldır.
+            System.out.println(true);
+
+        Employee employee=new Tester("ahmet",'M',42,"SDET","AMAZON",151,110000);
+
+        Developer developer=new Developer("ahmet",'M',42,"SDET","AMAZON",151,110000);
+
+
+        ((Developer)employee).fixBugs();  // will give error. because in this employee object type is tester.we wanna convert tester
+        //to developer. there is no is a relation
+
+
+    }
+}
